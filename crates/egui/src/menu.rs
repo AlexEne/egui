34a45ab0f051b3b328f55e9b64122100eec7980d
Alexandru Ene <alex.ene0x11@gmail.com>
2025-08-22 18:08:09 +0100

    Simple > for submenus

--- crates/egui/src/menu.rs
+++ crates/egui/src/menu.rs
@@ -602,7 +602,8 @@ impl SubMenu {
     fn new(parent_state: Arc<RwLock<MenuState>>, text: impl Into<WidgetText>) -> Self {
         let index = parent_state.write().next_entry_index();
         Self {
-            button: SubMenuButton::new(text, "⏵", index),
+            // button: SubMenuButton::new(text, "⏵", index),
+            button: SubMenuButton::new(text, ">", index),
             parent_state,
         }
     }
